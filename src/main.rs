22433e14cb02d0f071c76a7aeee831afe82eb365
Masterchef365 <duncan.freeman1@gmail.com>
2022-11-09 00:03:38 -0800

    Initial commit, working sim

---
+++ src/main.rs
@@ -0,0 +1,84 @@
+use std::{
+    fs::File,
+    io::{self, BufWriter, Write},
+    path::Path,
+};
+
+fn main() -> io::Result<()> {
+    let width = 1000 + 2;
+    let iters = 1000;
+    let mut grid = vec![false; width];
+
+    // Setup
+    grid[width / 2] = true;
+
+    // Rule
+    let rule = 30;
+
+    // Simulate
+    let image = sim(grid, iters, rule);
+
+    // Write image
+    //let image = pack_bools(&image);
+    write_pbm("out.pbm", width, iters, &image)
+}
+
+fn sim(mut grid: Vec<bool>, iters: usize, rule: u8) -> Vec<bool> {
+    let mut tmp = grid.clone();
+
+    let mut image = vec![];
+    for _ in 0..iters {
+        image.extend_from_slice(&grid);
+
+        for (grid, tmp) in grid.windows(3).zip(tmp.iter_mut().skip(1)) {
+            let n = bits_to_byte(grid);
+            *tmp = rule_lookup(rule, n);
+        }
+        std::mem::swap(&mut grid, &mut tmp);
+    }
+    image
+}
+
+fn rule_lookup(rule: u8, bin: u8) -> bool {
+    (rule >> bin) & 1 == 1
+}
+
+fn bits_to_byte(data: &[bool]) -> u8 {
+    assert!(data.len() <= u8::BITS as usize);
+    data.iter().fold(0, |mut acc, &x| {
+        acc <<= 1;
+        acc | u8::from(x)
+    })
+}
+
+/*
+fn pack_bools(data: &[bool]) -> Vec<u8> {
+    data.chunks(8).map(bits_to_byte).collect()
+}
+*/
+
+fn write_pbm(path: impl AsRef<Path>, width: usize, height: usize, data: &[bool]) -> io::Result<()> {
+    let mut f = BufWriter::new(File::create(path)?);
+    /*
+    let num_bits = data.len() * 8;
+    dbg!(num_bits, width, height);
+    assert!(
+        num_bits <= width * height,
+        "Image area is larger than provided data",
+    );
+    */
+    writeln!(f, "P1")?;
+    writeln!(f, "{} {}", width, height)?;
+    for row in data.chunks_exact(width) {
+        for &k in row {
+            if k {
+                write!(f, "0")?;
+            } else {
+                write!(f, "1")?;
+            }
+        }
+        writeln!(f)?;
+    }
+    //f.write_all(data)?;
+    Ok(())
+}
